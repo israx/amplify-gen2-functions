055774855775dcd5c7dbb78286d60c2677ff73ab
Israel Arcos <israel.arcos2595@gmail.com>
2024-02-18 10:11:23 -0500

    chore: add secret variable

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth, defineFunction } from "@aws-amplify/backend";
+import { defineAuth, defineFunction, secret } from "@aws-amplify/backend";
 /**
  * Define and configure your auth resource
  * When used alongside data, it is automatically configured as an auth provider for data
@@ -9,7 +9,13 @@ export const auth = defineAuth({
     email: true,
   },
   triggers: {
-    preSignUp: defineFunction({ entry: '../functions/pre-sign-up-handler.ts' }),
+    preSignUp: defineFunction({
+      entry: '../functions/pre-sign-up-handler.ts', 'environment': {
+        'MY_SECRET': secret('secrent-value'),
+        'MY_ENV_VAR': 'my-env-value'
+      }
+    },
+    ),
     createAuthChallenge: defineFunction({ entry: '../functions/create-auth-challenge-handler.ts' }),
     defineAuthChallenge: defineFunction({ entry: '../functions/define-auth-challenge-handler.ts' }),
     verifyAuthChallengeResponse: defineFunction({ entry: '../functions/verify-auth-challenge-handler.ts' })
